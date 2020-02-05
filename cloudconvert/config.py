9094abd828b25c7f729e0212b9d3066754b1bb0e
PyDev <pydeveloper455@gmail.com>
2020-02-06 00:57:29 +0530

    added config file for SDK

---
+++ cloudconvert/config.py
@@ -0,0 +1,9 @@
+__version__ = "2.0.0"
+__pypi_username__ = ""
+__pypi_packagename__ = "cloudconvert"
+__github_username__ = "cloudconvert"
+__github_reponame__ = "cloudconvert-python"
+__endpoint_map__ = {
+    "live": "https://api.cloudconvert.com",
+    "sandbox": "https://api.sandbox.cloudconvert.com"
+}
