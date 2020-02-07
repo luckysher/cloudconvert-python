8dbe758aa02999ab9a6de8966852624916b55357
PyDev <pydeveloper455@gmail.com>
2020-02-07 23:11:44 +0530

    removed operation param for job create

--- cloudconvert/job.py
+++ cloudconvert/job.py
@@ -1,7 +1,7 @@
-from cloudconvert.resource import List, Find, Create, Delete, Wait, Show, Resource
+from cloudconvert.resource import List, Find, Delete, Wait, Show, Resource, Create
 
 
-class Job(List, Find, Create, Wait, Show, Delete):
+class Job(List, Find, Wait, Show, Delete):
     """Job class wrapping the REST v2/jobs endpoint. Enabling New Job Creation, Showing a job, Waiting for job,
     Finding a job, Deleting a job.
 
@@ -12,6 +12,14 @@ class Job(List, Find, Create, Wait, Show, Delete):
         >>> Job.delete()<Job_ID>)     # return True or False
     """
     path = "v2/jobs"
+
+    @classmethod
+    def create(cls, payload={}):
+        res = Create.create(operation="jobs", payload=payload)
+        try:
+            return res['data']
+        except:
+            return res
 
 
 Job.convert_resources['jobs'] = Job
