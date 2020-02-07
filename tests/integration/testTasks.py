e19d49805d88e564dfef9d0ef8eeec91d22e2939
PyDev <pydeveloper455@gmail.com>
2020-02-07 12:52:13 +0530

    Added integration test for file

---
+++ tests/integration/testTasks.py
@@ -0,0 +1,67 @@
+###################################################################
+##      Test case for tasks                                      ##
+##                                                               ##
+## How to run ? :                                                ##
+##                $ python tests/integration/testTasks.py        ##
+###################################################################
+
+import sys
+import os
+sys.path.append(os.getcwd())
+
+import unittest
+import cloudconvert
+
+
+# API key for the Cloud convert Rest API
+API_KEY = "API_KEY"
+
+cloudconvert.API_KEY = API_KEY
+
+# Set API client in sandbox mode
+#cloudconvert.sandbox = True
+
+
+class TasksTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Test case setup method
+        :return:
+        """
+        print("Setting up task test case")
+        self.cloudconvert = cloudconvert
+
+        # setup the client with the provided API key by configuring
+        self.cloudconvert.configure()
+
+    def testImportUrlTask(self):
+        """
+        Test case for uploading file
+        :return:
+        """
+        print("Test case for 'import/url' file...")
+        new_task = {
+            "url": "file/url/to/upload"
+        }
+
+        task = cloudconvert.Task.create(operation="import/url", payload=new_task)
+
+        # do wait for the task
+        wait_task = cloudconvert.Task.find(id=task["data"]["id"])
+
+        # delete the task
+        deleted = cloudconvert.Task.delete(id=wait_task["data"]["id"])
+
+        print("task deleted with Id: {}".format(wait_task["data"]["id"]) if deleted else "unable to delete the task: {}".format(wait_task["data"]["id"]))
+
+    def tearDown(self):
+        """
+        Teardown method
+        :return:
+        """
+        print("Tearing down test case for task..")
+
+
+if __name__ == '__main__':
+    unittest.main()
